7e0d2db6f12e98c6dd0d7591626dc1a672b7b9ef
AbdessadekHm <abdessadekhm@gmail.com>
2025-02-22 16:48:28 +0100

    Add Home Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
